7ac853aee7b30d3b3227dcf2e30b9b67b2c8c041
Shatakshi Shree <91302103+shatakshii25@users.noreply.github.com>
2022-10-02 07:45:48 +0530

    Update authentication.js

--- src/util/authentication.js
+++ src/util/authentication.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
     const createToken = (data) => {
+        console.log(data.apiKey);
+        console.log(data.username);
         localStorage.username = data.username;
         localStorage.apiKey = data.apiKey;
     }
