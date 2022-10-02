d537060c89873a429e6902428e32fa05f0a804a6
shatakshii25 <shatakshishree25@gmail.com>
2022-10-02 09:04:03 +0530

    files changed

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -12,9 +12,12 @@ import { Navigate } from 'react-router-dom';
 const Dashboard = () => {
   const [shown, setShown] = useState(false);
   const [isShown, setIsShown] =useState(false);
+  const [shows, setShows] = useState(false);
   const [getData, setGetData] = useState()
   const [isSubmit, setIsSubmit] = useState(false);
+  const [isOtpSubmit, setIsOtpSubmit] = useState(false);
   const [postData, setPostData]= useState()
+  const [otpPostData, setOtpPostData]= useState()
   let navigate= useNavigate();
   const loggedIn = checkAuthentication();
 
@@ -41,12 +44,29 @@ const Dashboard = () => {
         }
       })
     }
-  }, [isSubmit])
+
+    if (isOtpSubmit){
+      otpPostData['username']=localStorage.username
+      otpPostData['apiKey']= localStorage.apiKey
+      axios.post('http://172.20.10.2:8080/verify/aadhaar/', otpPostData)
+      .then((res)=> {
+        console.log(res);
+        // console.log("token is: "+res.data.data)
+        // createToken(res.data.data)
+        if(checkAuthentication()){
+          navigate('/dashboard', {replace: true})
+        }
+      })
+    }
+  }, [isSubmit, isOtpSubmit])
 
   console.log(postData)
 
   
-
+const handleLogout=()=>{
+  clearToken()
+  navigate(0)
+}
   // console.log(paginatedData);
 
   
@@ -55,14 +75,14 @@ const Dashboard = () => {
       <div className="navbar">
         <img src={logo} className="logo" />
         <div className="user">
-          {/* <button type="button" onClick={clearToken()}>Logout</button> */}
           <img src="" />
         </div>
       </div>
       <div className="container">
         <div className="info">
           <h1>KnowMe</h1>
-          <p />
+          <button type="button" onClick={handleLogout }>Logout</button>
+          <br /><br />
           <button onClick={()=>setShown(true)}>Add an ID</button>
         </div>
       </div>
@@ -74,9 +94,44 @@ const Dashboard = () => {
             Name : {getData['aadhaar'].name} <br/>
             Gender : {getData['aadhaar'].gender} <br/>
             Verified : {getData['aadhaar'].isVerified ? '✔' : '❌'} <br/>
+            {!getData['aadhaar'].isVerified && (<span id="verify" onClick={()=>setShows(true)}>Verify</span>)}
           </div>
         )
       }
+      {shows && (
+        <Formik
+        initialValues={{otp:""}}
+        validationSchema = {
+          Yup.object({
+            otp : Yup.string()
+                  .required("Required")
+                  .matches(
+                    /^[0-9]{4}$/ ,
+                    "Must Contain 4 digits"
+                  )
+                  
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+                  setTimeout(() => {
+                    setShown(false);
+                    setSubmitting(false);
+                    setOtpPostData(values);
+                    setIsOtpSubmit(true);
+                  }, 400);
+                }}
+              >
+              <div className='form-1'>
+              <Form id='form'> 
+              <div className='form-group mt-2'>
+                        <Field name="otp" className="form-control red" placeholder="Enter OTP"></Field>
+                        <div className='errormessage' style={{ color: "red" }} >
+                        <ErrorMessage name="aadhaarNumber" className='error' /></div>
+                      </div>
+                      <button className="button" type='submit'><span>Submit</span></button>
+        </Form>
+        </div>
+        </Formik>
+      )}
       {shown && (
       <Formik
       initialValues={{aadhaarNumber:""}}
--- src/util/authentication.js
+++ src/util/authentication.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
     const createToken = (data) => {
         console.log(data.apiKey);
