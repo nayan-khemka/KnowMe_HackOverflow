733f099ba5aa0f6e275f375f6308d6b5427e2ef7
Shatakshi Shree <91302103+shatakshii25@users.noreply.github.com>
2022-10-02 07:46:18 +0530

    Update MyRoutes.js

--- src/Routes/MyRoutes.js
+++ src/Routes/MyRoutes.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import {Link, Routes, Route} from 'react-router-dom';
 import ProtectedRoutes from '../Components/ProtectedRoutes';
-import Allotment from '../Pages/Allotment';
 import Dashboard from '../Pages/Dashboard';
 import Login from '../Pages/Login';
-import Needy from '../Pages/Needy';
 import Register from '../Pages/Register';
-import Resources from '../Pages/Resources';
 
 const MyRoutes =()=> {
   return (
